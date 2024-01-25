bf0aa1467f5b80ff6adeb99f7f14e7082861c27b
Dimas Sudjito <almuhtad@ualberta.ca>
2024-01-25 16:45:17 -0700

    read arguments and number of threads

--- lab2/main.c
+++ lab2/main.c
@@ -6,9 +6,11 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<pthread.h>
+#include "common.h"
 
 /**
  * TODO:
+ * 0. Read input
  * 1. Handle multithreaded client (default=100)
  * 2. Create array of n strings
  * 3. Parse request and call read or write function
@@ -16,15 +18,11 @@
  * 5. Implement a better parallelization than mutex
 */
 
-void *ServerEcho(void *args)
-{
-    int clientFileDescriptor=(int)args;
-    char str[20];
+int NUM_STR_;
 
-    read(clientFileDescriptor,str,20);
-    printf("reading from client:%s\n",str);
-    write(clientFileDescriptor,str,20);
-    close(clientFileDescriptor);
+void *Process(void *args)
+{
+    printf("Do some task");
     return NULL;
 }
 
@@ -35,10 +33,14 @@ int main(int argc, char* argv[])
     int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
     int clientFileDescriptor;
     int i;
-    pthread_t t[20];
+    pthread_t t[COM_CLIENT_THREAD_COUNT];
 
-    sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
-    sock_var.sin_port=3000;
+    NUM_STR_ = strtol(argv[1], NULL, 10);
+    char *server_ip = argv[2];
+    int server_port = strtol(argv[3], NULL, 10);
+
+    sock_var.sin_addr.s_addr=inet_addr(server_ip);
+    sock_var.sin_port=server_port;
     sock_var.sin_family=AF_INET;
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
@@ -46,11 +48,11 @@ int main(int argc, char* argv[])
         listen(serverFileDescriptor,2000); 
         while(1)        //loop infinity
         {
-            for(i=0;i<20;i++)      //can support 20 clients at a time
+            for(i=0;i<COM_CLIENT_THREAD_COUNT;i++)
             {
                 clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
                 printf("Connected to client %d\n",clientFileDescriptor);
-                pthread_create(&t[i],NULL,ServerEcho,(void *)(long)clientFileDescriptor);
+                pthread_create(&t[i],NULL,Process,(void *)(long)clientFileDescriptor);
             }
         }
         close(serverFileDescriptor);
