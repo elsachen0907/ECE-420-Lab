241a064712ddc6f63c0f00dd2c6f830afb6dd04f
Mackenzie Malainey <macmalainey@gmail.com>
2024-02-29 18:43:04 -0700

    lab3: add openmp version of gauss algo

--- lab3/main.c
+++ lab3/main.c
@@ -2,13 +2,81 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <omp.h>
 
 #include "Lab3IO.h"
 #include "timer.h"
 
-int numThreads;
+void gauss(double** G, int n)
+{
+    double* temp = CreateVec(n);
 
-double** gauss(double** G, int n)
+    int row = 0;
+    double max = 0;
+
+    #pragma omp parallel shared(G, n, max, row, temp)
+    {
+        for (int k = 0; k < n - 1; k++) {
+            // double lmax = 0;
+            // int lrow = k;
+
+            #pragma omp for
+            for (int p = k; p < n; p++) {
+                double upk = fabs(G[p][k]);
+
+                // if (upk > lmax) {
+                //     lmax = upk;
+                //     lrow = k;
+                // }
+                if (upk > max) {
+                    #pragma omp critical
+                    {
+                        if (upk > max) {
+                            max = upk;
+                            row = k;
+                        }
+                    }
+                }
+            }
+
+            // if (lmax > max) {
+            //     #pragma omp critical
+            //     {
+            //         if (lmax > max) {
+            //             max = lmax;
+            //             row = lrow;
+            //         }
+            //     }
+            // }
+            // #pragma omp barrier
+
+            #pragma omp single
+            {
+                // Cleanup for next iteration
+                max = 0;
+                row = k + 1;
+
+                double* swap = G[row];
+                G[row] = G[k];
+                G[k] = swap;
+            }
+
+            #pragma omp for
+            for (int i = k + 1; i < n; i++) {
+                temp[i] = G[i][k] / G[k][k];
+            }
+
+            #pragma omp for collapse(2)
+            for (int i = k + 1; i < n; i++) {
+                for (int j = k; j < n + 1; j++) {
+                    G[i][j] = G[i][j] - temp[i] * G[k][j];
+                }
+            }
+        }
+    }
+}
+
+double** gauss_nomp(double** G, int n)
 {
     double** U = CreateMat(n, n + 1);
     for (int i = 0; i < n; i++) {
@@ -49,17 +117,22 @@ int main(int argc, char* argv[])
 {
     int size;
     double** G;
-    double** U;
+    double t1;
+    double t2;
 
     // Read command-line arguments
-    numThreads = strtol(argv[1], NULL, 10);
+    int numThreads = strtol(argv[1], NULL, 10);
+    omp_set_num_threads(numThreads);
 
-    // Load data 
+    // Load data
     Lab3LoadInput(&G, &size);
 
-    U = gauss(G, size);
+    GET_TIME(t1);
+    gauss(G, size);
+    GET_TIME(t2);
 
-    PrintMat(U, size, size + 1);
+    PrintMat(G, size, size + 1);
+    fprintf(stderr, "%f\n", t2 - t1);
 
     return 0;
 }
